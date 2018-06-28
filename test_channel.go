fea93cc902a480dff199d0789b20b7567883160f
maowei3 <maowei3@le.com>
2018-06-28 15:57:29 +0800

    update the file

--- test_channel.go
+++ test_channel.go
@@ -1,33 +1,35 @@
 package main
 
+//haha
 import "fmt"
 import "sync"
 import _ "net/http/pprof"
 import "net/http"
-var wg  sync.WaitGroup
-func Count(ch chan int,i int){
-     ch <- i
-     fmt.Println("count")
-     wg.Done()
+
+var wg sync.WaitGroup
+
+func Count(ch chan int, i int) {
+	ch <- i
+	fmt.Println("count")
+	wg.Done()
 }
 
+func main() {
+	wg.Add(11)
+	//chs := make([]chan int,10)
+	var chs = [10]chan int{}
+	for i := 0; i < 10; i++ {
+		chs[i] = make(chan int)
+		go Count(chs[i], i)
+	}
 
-func main(){
-     wg.Add(11)
-     //chs := make([]chan int,10)
-     var chs = [10]chan int{}
-     for i:=0;i< 10;i++{
-         chs[i] = make(chan int) 
-         go Count(chs[i],i)
-     }
-     
-     for _,ch := range(chs){
-          fmt.Println( <-ch)
-     }
-     
-     go func(){
-        http.ListenAndServe("0.0.0.0:8888",nil)
-     }()
-    
-     wg.Wait()
+	for _, ch := range chs {
+		fmt.Println(<-ch)
+	}
+
+	go func() {
+		http.ListenAndServe("0.0.0.0:8888", nil)
+	}()
+
+	wg.Wait()
 }
